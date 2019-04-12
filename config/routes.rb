9fd9058fdfa61d3941cd5e064103eca6abb3a400
Bijay Ranabhat <bijayranabhat_2019@depauw.edu>
2019-04-12 20:18:00 +0000

    added nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'pages#index'
 end
+ 
+ #git remote add origin youruser@yourserver.com:/path/to/my_project.git
